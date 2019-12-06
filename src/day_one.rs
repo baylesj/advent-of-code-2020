3bf81cd6b32109ee85c9116ecf2c9d2843c23c8c
Jordan Bayles <jophba@chromium.org>
2019-12-06 12:39:59 -0800

    Solve day 2

--- src/day_one.rs
+++ src/day_one.rs
@@ -8,19 +8,40 @@ type Mass = u32;
 type Fuel = u32;
 
 fn get_fuel_for_module(mass: Mass) -> Fuel {
-    ((mass as f32 / 3.0) - 2.0).trunc() as Fuel
+  let mut total_fuel: Fuel = 0;
+
+  let mut current_step_mass: Mass = mass;
+  while current_step_mass != 0 {
+    let current_step_fuel: Fuel = get_fuel_for_module_step(current_step_mass);
+    total_fuel += current_step_fuel;
+    current_step_mass = current_step_fuel as Mass;
+  }
+
+  total_fuel
+}
+
+fn get_fuel_for_module_step(mass: Mass) -> Fuel {
+  let one_third: Mass = (mass as f64 / 3.0).trunc() as Fuel;
+  if one_third > 2 {
+    one_third - 2
+  } else {
+    0
+  }
 }
 
 pub fn solve() -> String {
     let file = File::open(INPUT_FILENAME).expect("Invalid filename");
     let reader = BufReader::new(file);
 
-    let mut sum: Fuel = 0;
+    let mut part_one_sum: Fuel = 0;
+    let mut part_two_sum: Fuel = 0;
     for line in reader.lines() {
       let unparsed_mass: String = line.expect("Invalid file contents");
       let module_mass: Mass = unparsed_mass.parse::<Mass>()
           .expect("Invalid file contents");
-      sum += get_fuel_for_module(module_mass);
+          part_one_sum += get_fuel_for_module_step(module_mass);
+          part_two_sum += get_fuel_for_module(module_mass);
     }
-    sum.to_string()
+
+    format!("part one: {}, part two: {}", part_one_sum, part_two_sum)
 }
