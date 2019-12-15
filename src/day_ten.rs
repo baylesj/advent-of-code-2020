eb854af7e356e510d5e16eb962e83505bd5beaed
Jordan Bayles <jophba@chromium.org>
2019-12-14 16:12:14 -0800

    Switch to arctan

--- src/day_ten.rs
+++ src/day_ten.rs
@@ -1,5 +1,5 @@
-use fraction::Fraction;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
+use std::f64::consts::PI;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -40,16 +40,16 @@ impl LoadableFromFile for AsteroidMap {
 
 #[derive(Debug, Default)]
 pub struct VisiblePoint {
-  pub x: usize,
-  pub y: usize,
-  pub distance: f64
+    pub x: usize,
+    pub y: usize,
+    pub distance: f64,
 }
 
-pub type VisiblePoints = HashMap<String, Vec<VisiblePoint>>;
+pub type VisiblePoints = BTreeMap<String, Vec<VisiblePoint>>;
 
 // x equals column, y equals row. Top Left = (0, 0)
 pub fn calculate_visible_points(x: usize, y: usize, map: &AsteroidMap) -> VisiblePoints {
-    let mut found_slopes: VisiblePoints = HashMap::new();
+    let mut found_slopes: VisiblePoints = BTreeMap::new();
     for row in 0..map.len() {
         for col in 0..map[0].len() {
             if (x == col && y == row) || !map[row][col] {
@@ -59,21 +59,17 @@ pub fn calculate_visible_points(x: usize, y: usize, map: &AsteroidMap) -> Visibl
             let rise: i64 = y as i64 - row as i64;
             let run: i64 = x as i64 - col as i64;
 
-            // UR - 1, LR - 2, LL - 3, UL - 4
-            let quadrant: u8;
-            if rise >= 0 && run >= 0 {
-              quadrant = 1;
-            } else if rise < 0 && run >= 0 {
-              quadrant = 2;
-            } else if rise < 0 && run < 0 {
-              quadrant = 3;
-            } else {
-              quadrant = 4;
+            // UR - 4, LR - 3, LL - 2, UL - 1
+            let mut arctan: f64 = (rise as f64 / run as f64).atan();
+            if run < 0 {
+                arctan += PI;
             }
-            let slope = Fraction::new(rise.abs() as u64, run.abs() as u64);
-            let key = format!("{}-{}", quadrant, format!("{:.1$}", slope, 4));
+
+            let key = format!("{:.1$}", arctan, 9);
             let point = VisiblePoint {
-              x: col, y: row, distance: ((rise*rise + run*run) as f64).sqrt()
+                x: col,
+                y: row,
+                distance: ((rise * rise + run * run) as f64).sqrt(),
             };
 
             let points_with_same_slope = found_slopes.entry(key).or_insert(vec![]);
@@ -85,33 +81,52 @@ pub fn calculate_visible_points(x: usize, y: usize, map: &AsteroidMap) -> Visibl
 }
 
 pub fn get_best_location(input_filename: &str) -> VisiblePoints {
-  let map = AsteroidMap::load(input_filename);
-  let mut max_visible: i64 = 0;
-  let mut max_visible_points = VisiblePoints::default();
+    let map = AsteroidMap::load(input_filename);
+    let mut max_visible: i64 = 0;
+    let mut max_visible_points = VisiblePoints::default();
 
-  for row in 0..map.len() {
-      for col in 0..map[0].len() {
-          // No asteroid here, so can't place a station here.
-          if !map[row][col] {
-              continue;
-          }
+    for row in 0..map.len() {
+        for col in 0..map[0].len() {
+            // No asteroid here, so can't place a station here.
+            if !map[row][col] {
+                continue;
+            }
 
-          let visible_points = calculate_visible_points(col, row, &map);
-          if visible_points.len() as i64 > max_visible {
-            max_visible = visible_points.len() as i64;
-            max_visible_points = visible_points;
-          }
-      }
-  }
-  max_visible_points
+            let visible_points = calculate_visible_points(col, row, &map);
+            if visible_points.len() as i64 > max_visible {
+                max_visible = visible_points.len() as i64;
+                max_visible_points = visible_points;
+            }
+        }
+    }
+    max_visible_points
 }
 
 pub fn part_one(input_filename: &str) -> i64 {
-  get_best_location(input_filename).len() as i64
+    get_best_location(input_filename).len() as i64
 }
 
-pub fn part_two(_input_filename: &str) -> i64 {
-    128
+// Question is, what is the 200th asteroid to be vaporized?
+// Minus one to account for 0-indexing.
+const NTH_ASTEROID_PLACE: usize = 200 - 1;
+pub fn part_two(input_filename: &str) -> i64 {
+    let location = get_best_location(input_filename);
+    let sorted_keys: Vec<&String> = location.keys().rev().collect();
+
+    // Found experimentally.
+    // TODO: generalize to any case.
+    assert!(NTH_ASTEROID_PLACE < sorted_keys.len());
+
+    println!("sorted keys: {:#?}", sorted_keys);
+    let visible_point = location[sorted_keys[NTH_ASTEROID_PLACE]]
+        .iter()
+        .min_by(|a, b| a.distance.partial_cmp(&b.distance).expect("ordered"))
+        .unwrap();
+    println!(
+        "sorted keys: {:#?}",
+        location[sorted_keys[NTH_ASTEROID_PLACE]]
+    );
+    (visible_point.x * 100 + visible_point.y) as i64
 }
 
 pub fn solve() {
@@ -148,6 +163,6 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-      assert_eq!(part_one(INPUT_FILENAME), 278);
+        assert_eq!(part_one(INPUT_FILENAME), 278);
     }
 }
