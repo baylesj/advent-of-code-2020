c597142f24e0d8b7afeb8256bb7bc3b3b5b03bc0
Jordan Bayles <jophba@chromium.org>
2019-12-18 01:10:58 -0800

    Finished fourteen

--- src/day_fourteen.rs
+++ src/day_fourteen.rs
@@ -31,7 +31,6 @@ fn parse_reactions(input_filename: &str) -> HashMap<String, Reaction> {
         let mut reaction = Reaction::default();
         let l = line.expect("line should be valid");
 
-        // TODO: avoid copy?
         let ts: Vec<Reactant> = RE
             .captures_iter(&l)
             .map(|c| {
@@ -47,7 +46,6 @@ fn parse_reactions(input_filename: &str) -> HashMap<String, Reaction> {
             reaction.reactants.push(ts[i].clone());
         }
 
-        // We can move to ax + by + ... + cz = 0 form by "subtracting" the output.
         reaction.chemical = ts[ts_last].0.clone();
         reaction.quantity = ts[ts_last].1;
         reactions.insert(reaction.chemical.to_string(), reaction);
@@ -56,9 +54,7 @@ fn parse_reactions(input_filename: &str) -> HashMap<String, Reaction> {
     reactions
 }
 
-// TODO: perf is terrible.
 fn reduce_to_ore_to_fuel(reactions: &mut HashMap<String, Reaction>, amount: i64) -> i64 {
-    // start with FUEL, not with ORE
     let mut reduced_reactions = Vec::new();
     let mut current_reactions: Vec<Reactant> = vec![("FUEL".to_string(), amount)];
     let mut left_overs: HashMap<String, i64> = HashMap::new();
@@ -70,17 +66,20 @@ fn reduce_to_ore_to_fuel(reactions: &mut HashMap<String, Reaction>, amount: i64)
             let mut current_value;
             let mut value_count;
             if reaction.0 != "ORE" {
+                let rq = reactions[&reaction.0].quantity;
                 if left_overs.contains_key(&reaction.0) {
                     current_value = left_overs[&reaction.0];
                     value_count = 0;
                 } else {
-                    current_value = reactions[&reaction.0].quantity;
+                    current_value = rq;
                     value_count = 1;
                 }
 
-                while current_value < reaction.1 {
-                    current_value += reactions[&reaction.0].quantity;
-                    value_count += 1;
+                // LOL @ massive speed up from using while loop here.
+                if current_value < reaction.1 {
+                    let d = ((reaction.1 as f64 - current_value as f64) / rq as f64).ceil() as i64;
+                    current_value += rq * d;
+                    value_count += d;
                 }
 
                 left_overs.insert(reaction.0.to_string(), current_value - reaction.1);
@@ -108,8 +107,8 @@ pub fn part_one(input_filename: &str) -> i64 {
 pub fn part_two(input_filename: &str, goal: i64) -> i64 {
     let reactions = parse_reactions(input_filename);
 
-    let mut left = 4935420; // guess taken from part one.
-    let mut right = 5500000; // random guess.
+    let mut left = 1;
+    let mut right = 10000000;
     let mut mid = (left + right) / 2;
     while left != mid && mid != right {
         let mid_result = reduce_to_ore_to_fuel(&mut reactions.clone(), mid);
@@ -122,10 +121,6 @@ pub fn part_two(input_filename: &str, goal: i64) -> i64 {
         }
 
         mid = (left + right) / 2;
-        println!(
-            "left: {}, mid: {}, right: {}, res: {}",
-            left, mid, right, mid_result
-        );
     }
 
     mid
@@ -138,7 +133,10 @@ pub fn solve() {
         part_one(INPUT_FILENAME),
         part_two(INPUT_FILENAME, 1000000000000)
     );
-    println!("Time elapsed: {}ms", now.elapsed().as_millis());
+    println!(
+        "Time elapsed for day fourteen: {}ms",
+        now.elapsed().as_millis()
+    );
 }
 
 #[cfg(test)]
@@ -146,7 +144,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part_one_sample_one() {
-        assert_eq!(165, part_one("input/day_fourteen_sample_one.txt"))
+    fn test_part_one_sample_one() {
+        assert_eq!(165, part_one("input/day_fourteen_sample_one.txt"));
+    }
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(202617, part_one("input/day_fourteen.txt"));
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(7863863, part_two("input/day_fourteen.txt", 1000000000000));
     }
 }
