588b044dab15c830260759663803a6d4375053f0
Jordan Bayles <jophba@chromium.org>
2019-12-19 17:03:22 -0800

    progress on 17

--- src/day_seventeen.rs
+++ src/day_seventeen.rs
@@ -2,10 +2,17 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-const INPUT_FILENAME: &str = "input/day_.txt";
 
-pub fn part_one() -> i64 {
-    1
+#[path = "intcode_computer.rs"]
+mod intcode_computer;
+use intcode_computer::{LoadableFromFile, Program, ProgramState, Runnable};
+use queues::IsQueue;
+
+const INPUT_FILENAME: &str = "input/day_seventeen.txt";
+
+pub fn part_one(input_filename: &str) -> i64 {
+    let mut program = Program::load(input_filename);
+    program.run();
 }
 
 pub fn part_two() -> i64 {
@@ -13,5 +20,5 @@ pub fn part_two() -> i64 {
 }
 
 pub fn solve() -> String {
-    format!("part one: {}, part two: {}", part_one(), part_two())
+    format!("part one: {}, part two: {}", part_one(INPUT_FILENAME), part_two())
 }
