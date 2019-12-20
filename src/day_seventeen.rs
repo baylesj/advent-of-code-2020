358e1c66b87eb9e9a66c2bc820a9698c82c4328e
Jordan Bayles <jophba@chromium.org>
2019-12-19 17:34:26 -0800

    halted is screwy

--- src/day_seventeen.rs
+++ src/day_seventeen.rs
@@ -1,8 +1,3 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-
-
 #[path = "intcode_computer.rs"]
 mod intcode_computer;
 use intcode_computer::{LoadableFromFile, Program, ProgramState, Runnable};
@@ -13,6 +8,15 @@ const INPUT_FILENAME: &str = "input/day_seventeen.txt";
 pub fn part_one(input_filename: &str) -> i64 {
     let mut program = Program::load(input_filename);
     program.run();
+
+    let mut chars = vec!['0'; program.io.size() + 10];
+    for _ in 0..program.io.size() {
+        chars.push(program.io.remove().expect("io lies!") as u8 as char);
+    }
+    let s: String = chars.iter().collect();
+    println!("{}", s);
+    println!("state: {:?}", program.state);
+    1
 }
 
 pub fn part_two() -> i64 {
@@ -20,5 +24,9 @@ pub fn part_two() -> i64 {
 }
 
 pub fn solve() -> String {
-    format!("part one: {}, part two: {}", part_one(INPUT_FILENAME), part_two())
+    format!(
+        "part one: {}, part two: {}",
+        part_one(INPUT_FILENAME),
+        part_two()
+    )
 }
