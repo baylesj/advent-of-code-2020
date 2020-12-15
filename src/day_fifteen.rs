a7caf714dbbc4ab68f398ed6252745eb12a3b405
Jordan Bayles <jophba@chromium.org>
2020-12-15 01:57:19 -0800

    Finish day 15 completely

--- src/day_fifteen.rs
+++ src/day_fifteen.rs
@@ -5,36 +5,42 @@ use crate::loadable::LoadableFromFile;
 // this would probably be better as i32::MAX or similar.
 const MAXIMUM_SPOKEN_NUMBER: usize = 29473358;
 
-fn part_one(starting_numbers: &[i64], limit: usize) -> i64 {
-    let mut last_spoken_cache = vec![-1; MAXIMUM_SPOKEN_NUMBER + 1];
+// NOTE: use of i32 here, while annoying, causes 1/2 of memory to be used
+// and a subsequence ~250ms speedup in release.
+fn part_one(starting_numbers: &[i64], term: usize, cache: &mut Vec<i32>) -> i64 {
     for i in 0..starting_numbers.len() {
-        last_spoken_cache[starting_numbers[i] as usize] = i as i64;
+        cache[starting_numbers[i] as usize] = i as i32;
     }
-
     let mut last_spoken = 0;
-    for i in starting_numbers.len()..limit - 1 {
-        let last = last_spoken_cache[last_spoken as usize];
+    // The prompt is 1-indexed, but we are 0-indexed.
+    for i in starting_numbers.len()..term - 1 {
+        let last = cache[last_spoken as usize];
         let been_spoken_before = last != -1;
-        last_spoken_cache[last_spoken as usize] = i as i64;
+        cache[last_spoken as usize] = i as i32;
         last_spoken = if been_spoken_before {
-            i as i64 - last
+            i as i32 - last
         } else {
             0
         };
     }
-    last_spoken
+    last_spoken as i64
+}
+
+fn create_cache() -> Vec<i32> {
+    vec![-1; MAXIMUM_SPOKEN_NUMBER + 1]
 }
 
 pub fn solve() -> String {
+    let mut cache = create_cache();
     let numbers = Vec::<i64>::load("input/day_fifteen.txt");
     format!(
         "part one: {}, part two: {}",
-        part_one(&numbers, 2020),
+        part_one(&numbers, 2020, &mut cache.clone()),
         // Turns out the sequence has no cycles or patterns in variances after manual
         // inspection. Some digging shows that this sequence is actually Van Eck's,
         // which does not have a closed form. Performance comes from optimizations,
         // not reducing algorithmic complexity.
-        part_one(&numbers, 30000000)
+        part_one(&numbers, 30000000, &mut cache)
     )
 }
 
@@ -50,6 +56,7 @@ mod tests {
     #[test]
     fn test_example() {
         let numbers = Vec::<i64>::load("input/day_fifteen_example.txt");
-        assert_eq!(436, part_one(&numbers, 2020));
+        let mut cache = create_cache();
+        assert_eq!(436, part_one(&numbers, 2020, &mut cache));
     }
 }
