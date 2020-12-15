901da0dd2081777164c73bbbc209097c45054173
Jordan Bayles <jophba@chromium.org>
2020-12-14 18:19:10 -0800

    Fix test

--- src/day_thirteen.rs
+++ src/day_thirteen.rs
@@ -50,89 +50,24 @@ fn part_one(notes: &BusNotes) -> i64 {
     best_bus.1 * best_val
 }
 
-// Special thanks to:
-// https://math.stackexchange.com/questions/1322845/how-to-find-the-coefficients-of-bezouts-lemma
-fn compute_bezoit_coprime(a: i64, b: i64) -> (i64, i64) {
-    // Note on tuplet form: 0 is the ith term, 1 is the i-1th term.
-    let mut rs = (b, a);
-    let mut us = (0, 1);
-    let mut vs = (1, 0);
-    let mut quotient;
-    while rs.0 > 1 {
-        quotient = rs.1 / rs.0;
-        us = (us.1 - quotient * us.0, us.0);
-        vs = (vs.1 - quotient * vs.0, vs.0);
-        rs = (rs.1 - quotient * rs.0, rs.0);
-    }
-    (us.0, vs.0)
-}
-
-
 // Got a hint: Chinese Remainder Theorem
 // (https://en.wikipedia.org/wiki/Chinese_remainder_theorem)
 // Assumes everything is coprime, which makes sense otherwise one bus is
 // strictly a child of another bus.
 fn part_two(buses: &[(usize, i64)]) -> i64 {
-    // Constructive existence proof:
-    // x = a_1 mod n_1
-    // x = a_2 mod n_2
-    // bezout gives m_1*n_1 + m_2*n_2 = 1
-    // compute_bezoit_coprime -> m_1, m_2
-    // x = a_1*m_2*n_2 + a_2*m_1*n_1
-    // let first = notes.buses_in_service.first().unwrap();
-    // let mut a_prime: i64 = first.0 as i64;
-    // let mut mod_prime: i64 = first.1;
-
-    // for &bus in notes.buses_in_service.iter().skip(1) {
-    //     let bus_index = bus.0 as i64;
-    //     let bezoits = compute_bezoit_coprime(mod_prime, bus_index);
-    //     a_prime = a_prime * bezoits.1 * bus.1 + bus_index * bezoits.0 * mod_prime;
-    //     mod_prime *= bus.1;
-    // }
-    // a_prime
-
-    println!("buses: {:?}", buses);
-    // Big M is the resulting modulus/all of the modulo values (n) multiplied
-    // together.
-    let mut big_m = 1;
-    for bus in buses.iter() {
-        big_m *= bus.1;
+    let modulo_equations: Vec<(i64, i64)> =
+        buses.iter().map(|b| (-(b.0 as i64) % b.1, b.1)).collect();
+    // |r|emainder and |c|oefficient.
+    let mut rc = (0, 1);
+    for eq in &modulo_equations {
+        let coefficient = rc.1;
+        for k in 1..eq.1 {
+            if (coefficient * k) % eq.1 == 1 {
+                rc = ((((eq.0 - rc.0) * k) % eq.1) * rc.1 + rc.0, rc.1 * eq.1);
+            }
+        }
     }
-    println!("big m: {}", big_m);
-    let length = buses.len();
-
-    // ms are the big modulus divided by the respective modulo value,
-    // i.e. big m without the respective value in it.
-    let mut ms = vec![0; length];
-    for k in 0..length {
-        ms[k] = big_m / buses[k].1;
-    }
-
-    // mis are the inverse of the ms, which is m_k modulo n_k
-    let mut mis = vec![0; length];
-    for k in 0..length {
-        mis[k] = ms[k] % buses[k].1;
-    }
-
-    // out is the "Chinese Remainder", which is defined as the
-    // (sum_{n=0}^{n} (mi_k * m_k * a_k)) % big_m, and is
-    // the reaminder of the division some integer N by the product
-    // of the integers.
-    let mut out: i64 = 0;
-    for k in 0..length {
-        out += mis[k] * ms[k] * (buses[k].0) as i64
-    }
-    out %= big_m;
-    println!("ms: {:?}\nmis: {:?}\nout: {}", ms, mis, out);
-    for bus in buses {
-        println!(
-            "expected: {}, actual: {}, bus: {}",
-            bus.0,
-            out % bus.1,
-            bus.1
-        )
-    }
-    out
+    rc.0
 }
 
 pub fn solve() -> String {
@@ -150,10 +85,7 @@ mod tests {
 
     #[test]
     pub fn test_solve() {
-        // TODO: delete load and part_one() call and reenable solve when performance is fixed for part two.
-        let notes = BusNotes::load("input/day_thirteen.txt");
-        assert_eq!(3966, part_one(&notes));
-        //assert_eq!("part one: 3966, part two: 0", solve());
+        assert_eq!("part one: 3966, part two: 800177252346225", solve());
     }
 
     #[test]
@@ -166,16 +98,5 @@ mod tests {
     pub fn test_example_two() {
         let notes = BusNotes::load("input/day_thirteen_example.txt");
         assert_eq!(1068781, part_two(&notes.buses_in_service));
-    }
-
-    #[test]
-    pub fn internet_example() {
-        // https://www.dcode.fr/chinese-remainder
-        assert_eq!(23, part_two(&[(2, 3), (3, 5), (2, 7)]));
-    }
-
-    #[test]
-    pub fn test_bezoit() {
-        assert_eq!((-59, 1709), compute_bezoit_coprime(4258, 147));
     }
 }
