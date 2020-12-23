9158b620a3ad1dd906dc6f6ca7429c49156bb17c
Jordan Bayles <jophba@chromium.org>
2020-12-22 21:37:11 -0800

    Finish parsing for day 19

--- src/day_nineteen.rs
+++ src/day_nineteen.rs
@@ -1,16 +1,27 @@
 use crate::loadable::LoadableFromFile;
 use std::str::FromStr;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 #[derive(Debug, PartialEq)]
 enum Rule {
+    None,
     Single(char),
-    Referential(Vec<(usize, usize)>),
+    Referential(Vec<Vec<usize>>)
 }
 
 #[derive(Debug, PartialEq)]
 struct RuleAndIndex {
     index: usize,
     rule: Rule,
+}
+
+#[derive(Debug, Default)]
+struct Messages {
+    rules: HashMap<usize, Rule>,
+    messages: Vec<String>
 }
 
 impl FromStr for RuleAndIndex {
@@ -30,7 +41,7 @@ impl FromStr for RuleAndIndex {
             rule = Rule::Referential(
                 rest.split(" | ")
                     .map(|p| p.split_whitespace().collect())
-                    .map(|p: Vec<&str>| (p[0].parse().unwrap(), p[1].parse().unwrap()))
+                    .map(|p: Vec<&str>| p.iter().map(|r| usize::from_str(r).unwrap()).collect())
                     .collect(),
             );
         }
@@ -38,6 +49,29 @@ impl FromStr for RuleAndIndex {
             index: index,
             rule: rule,
         })
+    }
+}
+
+impl LoadableFromFile for Messages {
+    fn load(filename: &str) -> Self {
+        let file = File::open(filename).expect("Invalid filename");
+        let reader = BufReader::new(file);
+
+        let mut lines = reader.lines();
+        let mut rules = HashMap::new();
+        loop {
+            match lines.next().unwrap().unwrap().trim() {
+                "" => break,
+                s => {
+                    let r = RuleAndIndex::from_str(s).unwrap();
+                    rules.insert(r.index, r.rule);
+                }
+            }
+        }
+        Messages {
+            rules: rules,
+            messages: lines.map(|l| l.unwrap()).collect()
+        }
     }
 }
 
@@ -60,26 +94,37 @@ mod tests {
     #[test]
     fn test_from() {
         assert_eq!(
-            RuleAndIndex::from_str("36: \"b\"").unwrap(),
             RuleAndIndex {
                 index: 36,
                 rule: Rule::Single('b')
-            }
+            },
+            RuleAndIndex::from_str("36: \"b\"").unwrap(),
         );
         assert_eq!(
-            RuleAndIndex::from_str("6: 54 116").unwrap(),
             RuleAndIndex {
                 index: 6,
-                rule: Rule::Referential(vec![(54, 116)])
-            }
+                rule: Rule::Referential(vec![vec![54, 116]])
+            },
+            RuleAndIndex::from_str("6: 54 116").unwrap(),
         );
         assert_eq!(
-            RuleAndIndex::from_str("93: 99 100 | 36 8").unwrap(),
             RuleAndIndex {
                 index: 93,
-                rule: Rule::Referential(vec![(99, 100), (36, 8)])
-            }
+                rule: Rule::Referential(vec![vec![99, 100], vec![36, 8]])
+            },
+            RuleAndIndex::from_str("93: 99 100 | 36 8").unwrap(),
         );
+    }
+
+    #[test]
+    fn test_load() {
+        let messages = Messages::load("input/day_nineteen_example.txt");
+        assert_eq!(Rule::Referential(vec![vec![4, 1, 5]]),
+        messages.rules[&0]);
+        assert_eq!(Rule::Single('a'),
+        messages.rules[&4]);
+
+        assert_eq!("ababbb", messages.messages[0]);
     }
 
     #[test]
